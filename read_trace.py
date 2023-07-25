116b96d24c6885e4f83490baf4d8c6c80c20ed9e
Shraddha Barke <sbarke@eng.ucsd.edu>
2023-07-25 22:16:33 +0530

    read trace: handle case where obj_id not in pred dict

--- read_trace.py
+++ read_trace.py
@@ -10,7 +10,7 @@ class Transition:
 def read_trace(name: str, obj_id: int) -> list[Transition]:
     '''
     Extract a single trace from a CSV file into a list of transitions.
-    
+
     Example:
     >>> # get the traces for the three particles in gravity_i
     >>> traces = [read_trace('gravity_i', obj_id) for obj_id in (5, 6, 7)]
@@ -21,9 +21,13 @@ def read_trace(name: str, obj_id: int) -> list[Transition]:
     def process_pred(line):
         name, vals = line
         vals = ast.literal_eval(vals)
-        if isinstance(vals, dict):
+        if isinstance(vals, dict) and str(obj_id) in vals.keys():
             vals = vals[str(obj_id)]
+        elif isinstance(vals, dict) and str(obj_id) not in vals.keys():
+            return None
         return [bool(x) for x in vals]
-    preds = [process_pred(l) for l in csv.reader(open(f'traces/{name}predicate.csv'), dialect)]
+
+    preds = [process_pred(l) for l in csv.reader(open(f'traces/{name}predicate.csv'), dialect) if process_pred(l) is not None]
     return list(map(lambda actions, *bits: Transition(actions, list(bits)), actions, *preds))
 
+print(read_trace('paint', 7))
