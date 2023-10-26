3ef684c5c021626887d7739a702419df41e0187b
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2023-10-25 18:42:06 -0700

    Fix encoding bugs

--- baseline_both.py
+++ baseline_both.py
@@ -15,10 +15,12 @@ def declare(trace, max_lines):
         for a in t.possible_actions:
             if a not in action_ids:
                 action_ids[a] = len(action_ids)
+
     N, preds = len(trace), len(trace[0].bits) # number of frames # number of predicates
     Pred = BitVecSort(len(trace[0].bits))
     K = len(action_ids.keys()) # number of all possible actions 
     states = [Int(f'state_{i}') for i in range(N+1)]
+
     # line can either correspond to action or state; and can be split on predicates or states. 
     # Children are always one of the possible lines!
     LineType = Datatype('LineType')
@@ -41,10 +43,9 @@ def declare(trace, max_lines):
     for j in range(max_lines):
         line = line_fn(j)
         # if line-type is action, ensure it's within the range of possible action ids/predicates
-        s.add(Implies(LineType.is_Action(line), And(LineType.action_val(line) >= 0, LineType.action_val(line) < len(action_ids))))
+        s.add(Implies(LineType.is_Action(line), And(LineType.action_val(line) >= 0, LineType.action_val(line) < K)))
         s.add(Implies(LineType.is_BranchPred(line), And(LineType.branch_pred(line) >= 0, LineType.branch_pred(line) < preds)))
-        if j >= 1:
-            s.add(Implies(Or(LineType.is_BranchPred(line), LineType.is_BranchState(line)),
+        s.add(Implies(Or(LineType.is_BranchPred(line), LineType.is_BranchState(line)),
             And(And(0 <= left_child(j), left_child(j) < j), And(0 <= right_child(j), right_child(j) < j))))
 
     for j in range(0, max_lines):
@@ -60,23 +61,24 @@ def declare(trace, max_lines):
 
             s.add(Implies(is_action_condition, line_val(j, frame_bv, states[i]) == line_fn(j)))
             s.add(Implies(is_state_condition, line_val(j, frame_bv, states[i]) == line_fn(j)))
-            possible_pred_vals = [i for i in range(preds)]
-            for idx in possible_pred_vals: # evaluating all possible predicates
+
+            for idx in range(preds): # evaluating all possible predicates
                 s.add(Implies(And(is_branchpred_condition, branch_pred == idx, Extract(idx, idx, frame_bv) == 1),
                           line_val(j, frame_bv, states[i]) == line_val(left_child(j), frame_bv, states[i])))
                 s.add(Implies(And(is_branchpred_condition, branch_pred == idx, Extract(idx, idx, frame_bv) == 0),
                           line_val(j, frame_bv, states[i]) == line_val(right_child(j), frame_bv, states[i])))
-
-                s.add(If(And(is_branchstate_condition, branch_state == states[i]),
-                        line_val(j, frame_bv, states[i]) == line_val(left_child(j), frame_bv, states[i]),
-                        line_val(j, frame_bv, states[i]) == line_val(right_child(j), frame_bv, states[i])))
+                
+            s.add(Implies(is_branchstate_condition,
+                          line_val(j, frame_bv, states[i]) == 
+                              If(states[i] == branch_state, 
+                                 line_val(left_child(j), frame_bv, states[i]), 
+                                 line_val(right_child(j), frame_bv, states[i]))))
 
     # evaluate correctly
     for i in range(N):
         frame_bv = bits_to_bv(trace[i].bits)
         possible_actions = [action_ids[a] for a in trace[i].possible_actions]
         # Top Action - action_val could belong to one of the possible actions
-        possible_actions = [action_ids[a] for a in trace[i].possible_actions]
         s.add(And(LineType.is_Action(line_val(max_lines - 2, frame_bv, states[i])),
         Or([LineType.action_val(line_val(max_lines - 2, frame_bv, states[i])) ==
             action for action in possible_actions])))
